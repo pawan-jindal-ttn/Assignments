ef6ddc12b6c6f4bdc49fb2e6d767b333dc4b67ce
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-02-12 09:51:21 +0530

    Modified Ques6

--- src/Ques6/Prog1.java
+++ src/Ques6/Prog1.java
@@ -7,17 +7,16 @@ import java.util.Scanner;
 public class Prog1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter date 1 (yyyy-MM-dd): ");
+        System.out.print("Enter date 1 (yyyy-MM-dd): ");
         String date1 = input.nextLine();
-        System.out.println("Enter date 2 (yyyy-MM-dd): ");
+        System.out.print("Enter date 2 (yyyy-MM-dd): ");
         String date2 = input.nextLine();
 
         LocalDate date_one = LocalDate.parse(date1, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         LocalDate date_two = LocalDate.parse(date2, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
 
-        if(date_one != date_two){
-            System.out.println("First Date is: " + date_one);
-            System.out.println("Second Date is: " + date_two);
-        }
+        System.out.println("First Date is after Second Date: " + date_one.isAfter(date_two));
+        System.out.println("First Date is before Second Date: " + date_one.isBefore(date_two));
+
     }
 }
